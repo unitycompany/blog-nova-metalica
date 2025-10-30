953076822d3866464ab52a1c0002d74f68da74a8
Aleph Ramos <alephsramosdev@gmail.com>
2025-10-30 17:24:21 -0300

    commit rápido

--- src/pages/blog/post/index.tsx
+++ src/pages/blog/post/index.tsx
@@ -1,21 +1,12 @@
-import styled from '@emotion/styled';
-import PostSlug from './[slug]';
+import type { GetServerSideProps } from 'next'
 
-const ContainerArticle = styled.article`
-  width: 100%;
-  height: auto;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`
-
-export default function Post() {
-  
-  return (
-    <>
-      <ContainerArticle>
-        <PostSlug />
-      </ContainerArticle>
-    </>
-  );
+export default function PostIndexRedirect() {
+  return null
 }
+
+export const getServerSideProps: GetServerSideProps = async () => ({
+  redirect: {
+    destination: '/blog',
+    permanent: false,
+  },
+})
